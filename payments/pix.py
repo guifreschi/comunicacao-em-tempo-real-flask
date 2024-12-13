c1f0c9539d9bd64e61681262be414f20b5a4dbbd
Guilherme Freschi <guilhermefreschix@gmail.com>
2024-12-13 16:39:07 -0300

    Store QR code image and create route to display it

--- payments/pix.py
+++ payments/pix.py
@@ -7,7 +7,7 @@ class Pix:
 
   def create_payment(self):
     # Criar o pagamento na instituição financeira
-    bank_payment_id = uuid.uuid4()
+    bank_payment_id = str(uuid.uuid4())
 
     # Código copia e cola
     hash_payment = f'hash_payment_{bank_payment_id}'
@@ -20,5 +20,5 @@ class Pix:
 
     return {
       "bank_payment_id": bank_payment_id,
-      "qr_code_path": f"static/img/qr_code_payment_{bank_payment_id}"
+      "qr_code_path": f"qr_code_payment_{bank_payment_id}"
     }
