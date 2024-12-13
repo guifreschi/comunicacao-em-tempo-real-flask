d4277ccbdd73d374c018bc7dcd8dfc7b22bf6e4f
Guilherme Freschi <guilhermefreschix@gmail.com>
2024-12-13 17:26:35 -0300

    Create payment page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, send_file
+from flask import Flask, jsonify, request, send_file, render_template
 from repository.database import db
 from db_models.payment import Payment
 from datetime import datetime, timedelta
@@ -10,7 +10,6 @@ app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
 
 db.init_app(app)
 
-@app.route('/payments/pix', methods=["POST"])
 @app.route('/payments/pix', methods=["POST"])
 def create_payment_pix():
   data = request.get_json()
@@ -44,7 +43,7 @@ def pix_confirmation():
 
 @app.route('/payments/pix/<int:payment_id>', methods=["GET"])
 def payment_pix_page(payment_id):
-  return 'pagamento pix'
+  return render_template('payment.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
