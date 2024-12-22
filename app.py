3972137c5dcca86da21910e575ba595c2a70bc3e
Guilherme Freschi <guilhermefreschix@gmail.com>
2024-12-22 16:29:44 -0300

    Notifying the client of confirmed payment

--- app.py
+++ app.py
@@ -56,12 +56,17 @@ def pix_confirmation():
   
   payment.paid = True
   db.session.commit()
+  socketio.emit(f'payment-confirmed-{payment.id}')
 
   return jsonify({"message": "The payment has been confirmed."})
 
 @app.route('/payments/pix/<int:payment_id>', methods=["GET"])
 def payment_pix_page(payment_id):
   payment = Payment.query.get(payment_id)
+  
+  if payment.paid:
+    return render_template('confirmed_payment.html', payment_id=payment.id, value=payment.value)
+
   return render_template('payment.html', payment_id=payment.id, value=payment.value, host='http://127.0.0.1:5000', qr_code=payment.qr_code)
 
 # websockets
